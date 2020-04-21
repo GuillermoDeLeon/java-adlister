9f3606a391585d97e2ca2faf9b2249307e57e54b
Guillermo De Leon <guillermo.x.deleon@gmail.com>
2020-04-21 11:57:58 -0500

    updated info to helloworldservlet

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -8,7 +8,8 @@ import java.io.PrintWriter;
 
 @WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello-world")
 public class HelloWorldServlet extends HttpServlet {
-//    protected void doGet(HttpServletRequest req, HttpServletResponse res)
+
+        @Override
         protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
             response.setContentType("text/html");
             PrintWriter out = response.getWriter();
@@ -16,4 +17,31 @@ public class HelloWorldServlet extends HttpServlet {
         }
 
 }
+
+//from Vivian
+//
+//@WebServlet(name = "HelloNameServlet", urlPatterns = "/hello-name")
+//public class HelloNameServlet extends HttpServlet {
+//    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        String pageHtml = "<h1>Hello, World!</h1>";
+//        //VERSION ONE
+//        //checks to see if parameter of name exists
+//
+//        if (request.getParameterMap().containsKey("name")) {
+//
+//            //sets name to String name
+//            String name = request.getParameter("name");
+//
+//            if (!name.equals("")) {
+//                System.out.println(name);
+//                pageHtml = "<h1>Hello, " + name + "!</h1>";
+//            }
+//        }
+//
+//        response.setContentType("text/html");
+//        PrintWriter out = response.getWriter();
+//        out.println(pageHtml);
+//
+//    }
+
 
