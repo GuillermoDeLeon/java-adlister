c4a7eb7dff8436e2160a270db6309ff5e98a8346
Guillermo De Leon <guillermo.x.deleon@gmail.com>
2020-04-30 14:56:40 -0500

    jdbc exercises

--- src/main/java/DaoFactory.java
+++ src/main/java/DaoFactory.java
@@ -1,9 +1,21 @@
+import java.sql.SQLException;
+
 public class DaoFactory {
     private static Ads adsDao;
+    private static Config config;
+
+    static {
+        try {
+            config = new Config();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static Ads getAdsDao() {
         if (adsDao == null) {
-            adsDao = new ListAdsDao();
+//            adsDao = new ListAdsDao();
+            adsDao = new MySQLAdsDao(config);
         }
         return adsDao;
     }
